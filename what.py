45677305f6346a1a58825a43dce01e53b2e42474
SubediRakshya <subedr1@unlv.nevada.edu>
2025-10-14 13:14:58 -0700

    Update what.py_read_write_for xm model

--- what.py
+++ what.py
@@ -1,146 +1,140 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-# Copyright 2021 ROBOTIS CO., LTD.
+################################################################################
+# Copyright 2017 ROBOTIS CO., LTD.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# Author: Wonho Yun, Will Son
+# ...
+################################################################################
 
+# Author: Ryu Woon Jung (Leon)
 
-from dynamixel_sdk import COMM_SUCCESS
-from dynamixel_sdk import PacketHandler
-from dynamixel_sdk import PortHandler
-from dynamixel_sdk_custom_interfaces.msg import SetPosition
-from dynamixel_sdk_custom_interfaces.srv import GetPosition
-import rclpy
-from rclpy.node import Node
-from rclpy.qos import QoSProfile
+#
+# *********     Read and Write Example      *********
+#
+# Available DXL model on this example : All models using Protocol 2.0
+# This example is tested with a DXL XM430, and an USB2DYNAMIXEL
+# Be sure that DXL XM430 properties are already set as %% ID : 1 / Baudnum : 34 (Baudrate : 57600)
+#
+
+import os
+
+if os.name == 'nt':
+    import msvcrt
+    def getch():
+        return msvcrt.getch().decode()
+else:
+    import sys, tty, termios
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    def getch():
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+from dynamixel_sdk import *                    # Uses Dynamixel SDK library
 
 # Control table address
-ADDR_OPERATING_MODE = 11  # Control table address is different in Dynamixel model
-ADDR_TORQUE_ENABLE = 64
-ADDR_GOAL_POSITION = 116
-ADDR_PRESENT_POSITION = 132
+ADDR_MX_TORQUE_ENABLE      = 64
+ADDR_MX_GOAL_POSITION      = 116
+ADDR_MX_PRESENT_POSITION   = 132
 
 # Protocol version
-PROTOCOL_VERSION = 2.0  # Default Protocol version of DYNAMIXEL X series.
+PROTOCOL_VERSION            = 2.0               # Changed for XM430
 
-# Default settings
-DXL_ID = 1  # Dynamixel ID : 1
-BAUDRATE = 57600  # Dynamixel default baudrate : 57600
-DEVICE_NAME = '/dev/ttyUSB0'  # Check which port is being used on your controller
-TORQUE_ENABLE = 1  # Value for enabling the torque
-TORQUE_DISABLE = 0  # Value for disabling the torque
-POSITION_CONTROL = 3  # Value for position control mode
+# Default setting
+DXL_ID                      = 1
+BAUDRATE                    = 57600
+DEVICENAME                  = '/dev/ttyUSB0'
 
+TORQUE_ENABLE               = 1
+TORQUE_DISABLE              = 0
+DXL_MINIMUM_POSITION_VALUE  = 10
+DXL_MAXIMUM_POSITION_VALUE  = 4000
+DXL_MOVING_STATUS_THRESHOLD = 20
 
-class ReadWriteNode(Node):
+index = 0
+dxl_goal_position = [DXL_MINIMUM_POSITION_VALUE, DXL_MAXIMUM_POSITION_VALUE]
 
-    def __init__(self):
-        super().__init__('read_write_node')
+# Initialize PortHandler instance
+portHandler = PortHandler(DEVICENAME)
 
-        self.port_handler = PortHandler(DEVICE_NAME)
-        self.packet_handler = PacketHandler(PROTOCOL_VERSION)
+# Initialize PacketHandler instance
+packetHandler = PacketHandler(PROTOCOL_VERSION)
 
-        if not self.port_handler.openPort():
-            self.get_logger().error('Failed to open the port!')
-            return
-        self.get_logger().info('Succeeded to open the port.')
+# Open port
+if portHandler.openPort():
+    print("Succeeded to open the port")
+else:
+    print("Failed to open the port")
+    print("Press any key to terminate...")
+    getch()
+    quit()
 
-        if not self.port_handler.setBaudRate(BAUDRATE):
-            self.get_logger().error('Failed to set the baudrate!')
-            return
-        self.get_logger().info('Succeeded to set the baudrate.')
+# Set port baudrate
+if portHandler.setBaudRate(BAUDRATE):
+    print("Succeeded to change the baudrate")
+else:
+    print("Failed to change the baudrate")
+    print("Press any key to terminate...")
+    getch()
+    quit()
 
-        self.setup_dynamixel(DXL_ID)
-        qos = QoSProfile(depth=10)
-
-        self.subscription = self.create_subscription(
-            SetPosition,
-            'set_position',
-            self.set_position_callback,
-            qos
-        )
-
-        self.srv = self.create_service(GetPosition, 'get_position', self.get_position_call>
-
-    def setup_dynamixel(self, dxl_id):
-        dxl_comm_result, dxl_error = self.packet_handler.write1ByteTxRx(
-            self.port_handler, dxl_id, ADDR_OPERATING_MODE, POSITION_CONTROL
-        )
+# Enable Dynamixel Torque
+dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(
+    portHandler, DXL_ID, ADDR_MX_TORQUE_ENABLE, TORQUE_ENABLE)
 if dxl_comm_result != COMM_SUCCESS:
-            self.get_logger().error(f'Failed to set Position Control Mode: \
-                                    {self.packet_handler.getTxRxResult(dxl_comm_result)}')
-        else:
-            self.get_logger().info('Succeeded to set Position Control Mode.')
+    print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+elif dxl_error != 0:
+    print("%s" % packetHandler.getRxPacketError(dxl_error))
+else:
+    print("Dynamixel has been successfully connected")
 
-        dxl_comm_result, dxl_error = self.packet_handler.write1ByteTxRx(
-            self.port_handler, dxl_id, ADDR_TORQUE_ENABLE, TORQUE_ENABLE
-        )
+while 1:
+    print("Press any key to continue! (or press ESC to quit!)")
+    if getch() == chr(0x1b):
+        break
+
+    # Write goal position (changed to 4-byte for XM430)
+    dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(
+        portHandler, DXL_ID, ADDR_MX_GOAL_POSITION, dxl_goal_position[index])
+    if dxl_comm_result != COMM_SUCCESS:
+        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+    elif dxl_error != 0:
+        print("%s" % packetHandler.getRxPacketError(dxl_error))
+
+    while 1:
+        # Read present position (changed to 4-byte for XM430)
+        dxl_present_position, dxl_comm_result, dxl_error = packetHandler.read4ByteTxRx(
+            portHandler, DXL_ID, ADDR_MX_PRESENT_POSITION)
         if dxl_comm_result != COMM_SUCCESS:
-            self.get_logger().error(f'Failed to enable torque: \
-                                    {self.packet_handler.getTxRxResult(dxl_comm_result)}')
-        else:
-            self.get_logger().info('Succeeded to enable torque.')
-
-    def set_position_callback(self, msg):
-        goal_position = msg.position
-
-        dxl_comm_result, dxl_error = self.packet_handler.write4ByteTxRx(
-            self.port_handler, msg.id, ADDR_GOAL_POSITION, goal_position
-        )
-
-        if dxl_comm_result != COMM_SUCCESS:
-            self.get_logger().error(f'Error: \
-                                    {self.packet_handler.getTxRxResult(dxl_comm_result)}')
+            print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
         elif dxl_error != 0:
-            self.get_logger().error(f'Error: {self.packet_handler.getRxPacketError(dxl_err>
-        else:
-            self.get_logger().info(f'Set [ID: {msg.id}] [Goal Position: {msg.position}]')
+            print("%s" % packetHandler.getRxPacketError(dxl_error))
 
-    def get_position_callback(self, request, response):
-        dxl_present_position, dxl_comm_result, dxl_error = self.packet_handler.read4ByteTx>
-            self.port_handler, request.id, ADDR_PRESENT_POSITION
-        )
+        print("[ID:%03d] GoalPos:%03d  PresPos:%03d" % (
+            DXL_ID, dxl_goal_position[index], dxl_present_position))
 
-        if dxl_comm_result != COMM_SUCCESS:
-            self.get_logger().error(f'Error: {self.packet_handler.getTxRxResult(dxl_comm_r>
-        elif dxl_error != 0:
-            self.get_logger().error(f'Error: {self.packet_handler.getRxPacketError(dxl_err>
-        else:
+        if not abs(dxl_goal_position[index] - dxl_present_position) > DXL_MOVING_STATUS_THRESHOLD:
+            break
 
-self.get_logger().info(f'Get [ID: {request.id}] \
-                                   [Present Position: {dxl_present_position}]')
+    # Change goal position
+    if index == 0:
+        index = 1
+    else:
+        index = 0
 
-        response.position = dxl_present_position
-        return response
+# Disable Dynamixel Torque
+dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(
+    portHandler, DXL_ID, ADDR_MX_TORQUE_ENABLE, TORQUE_DISABLE)
+if dxl_comm_result != COMM_SUCCESS:
+    print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+elif dxl_error != 0:
+    print("%s" % packetHandler.getRxPacketError(dxl_error))
 
-    def __del__(self):
-        self.packet_handler.write1ByteTxRx(self.port_handler,
-                                           1,
-                                           ADDR_TORQUE_ENABLE,
-                                           TORQUE_DISABLE)
-        self.port_handler.closePort()
-        self.get_logger().info('Shutting down read_write_node')
-
-
-def main(args=None):
-    rclpy.init(args=args)
-    node = ReadWriteNode()
-    rclpy.spin(node)
-    node.destroy_node()
-    rclpy.shutdown()
-
-
-if __name__ == '__main__':
-    main()
+# Close port
+portHandler.closePort()
