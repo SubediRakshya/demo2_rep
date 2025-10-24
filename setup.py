d2c7b88d4fb83bcc06128be5c69f2905661330c2
SubediRakshya <subedr1@unlv.nevada.edu>
2025-10-24 12:41:55 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,22 +10,19 @@ setup(
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+         ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        # Include .srv files
-        (os.path.join('share', package_name, 'srv'), glob('srv/*.srv')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='Your Name',
-    maintainer_email='your@email.com',
-    description='Control Dynamixel motors using client-server communication in ROS 2.',
+    maintainer='your_name',
+    maintainer_email='your_email@example.com',
+    description='Dynamixel motor control with ROS2 service-client structure',
     license='Apache License 2.0',
-    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'rotate_motor_client = dynamixel_control.rotate_motor_client:main',
-            'rotate_motor_service = dynamixel_control.rotate_motor_service:main',
+            'motor_service = dynamixel_control.motor_service:main',
+            'motor_client = dynamixel_control.motor_client:main',
         ],
     },
 )
